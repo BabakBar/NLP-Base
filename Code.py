2f1231d8a9cc28fad09c35bae2aaf20c27cd2003
BabakBar <babak.barghi@gmail.com>
2021-12-30 03:13:19 +0100

    added

--- Code.py
+++ Code.py
@@ -11,4 +11,5 @@ print(result)
 my_string = "Let's write RegEx!  Won't that be fun?  I sure think so.  Can you find 4 sentences?  Or perhaps, all 19 words?"
 
 # Write a pattern to match sentence endings: sentence_endings
-sentence_endings = r"[___]"
+sentence_endings = (r"[.?!]")
+print(re.split(sentence_endings, my_string))
