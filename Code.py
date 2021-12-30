dcb1e4f5da1eddf4d8b8b11e5e0de08ca7027407
BabakBar <babak.barghi@gmail.com>
2021-12-30 03:02:42 +0100

    cool

--- Code.py
+++ Code.py
@@ -5,3 +5,10 @@ import re
 my_string = "Let's write RegEx!"
 result = re.findall(r"\w+", my_string)
 print(result)
+
+# re.split() and re.findall()
+
+my_string = "Let's write RegEx!  Won't that be fun?  I sure think so.  Can you find 4 sentences?  Or perhaps, all 19 words?"
+
+# Write a pattern to match sentence endings: sentence_endings
+sentence_endings = r"[___]"
